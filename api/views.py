095d9053378698d5e8ee0ad14f20d4d3ce4d4780
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-07-05 07:39:55 +0530

    added rest_api with 2 endpoints

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(req):
 
     routes = [
@@ -40,4 +42,18 @@ def getRoutes(req):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def getNotes(req):
+    notes = Note.objects.all()
+    serialzer = NoteSerializer(notes, many=True)
+    return Response(serialzer.data)
+
+
+@api_view(["GET"])
+def getNote(req, id):
+    notes = Note.objects.get(id=id)
+    serialzer = NoteSerializer(notes, many=False)
+    return Response(serialzer.data)
